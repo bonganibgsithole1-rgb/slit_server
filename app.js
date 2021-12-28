144a8c5a432ab6289283f72d35dd881423f5c4f8
Bongani Sithole <bonganibgsithole@gmail.com>
2021-12-28 22:18:09 +0200

    The placement of the routes is done

--- app.js
+++ app.js
@@ -7,7 +7,15 @@ app.set("view engine", "ejs");
 app.get("/", function (req, res) {
   res.render("pages/01_index.ejs");
 });
-
-app.listen("3050", function () {
+app.get("/board", function (req, res) {
+  res.render("pages/02_board.ejs");
+});
+app.get("/donate", function (req, res) {
+  res.render("pages/03_donate.ejs");
+});
+app.get("/chapters", function (req, res) {
+  res.render("pages/04_course(landing).ejs");
+});
+app.listen("3000", function () {
   console.log("Server is connected");
 });
