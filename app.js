88457609c28f8358c4742a84f64b2d501794c20f
Bongani Sithole <bonganibgsithole@gmail.com>
2021-12-28 19:04:32 +0200

    done arranging and uploading the index route

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 let express = require("express");
 let app = express();
 
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
 app.get("/", function (req, res) {
-  res.send("hello there");
+  res.render("pages/01_index.ejs");
 });
-app.listen("3000", function () {
+
+app.listen("3050", function () {
   console.log("Server is connected");
 });
