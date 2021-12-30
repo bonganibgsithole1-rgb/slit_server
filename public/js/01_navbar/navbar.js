857f3b39bf603a7385358194263124a5c8a94083
Thokozani Msebele <thoko@byosoftware.co.zw>
2021-12-30 16:49:34 +0200

    changed the colours of svg

--- public/js/01_navbar/navbar.js
+++ public/js/01_navbar/navbar.js
@@ -3,11 +3,11 @@ for (let i = 0; i < arr.length; i++) {
   let popup = ` #navbarSection #${arr[i]}PopUp`;
   let liAndPopup = `#navbarSection .navList #${arr[i]}Li, ${popup}`;
   $(liAndPopup).on("mouseover click", function () {
-    $(popup).css({ display: "flex" });
+    $(popup).css({ display: "block" });
   });
-  //   $(liAndPopup).on("mouseout", function () {
-  //     $(popup).css({ display: "none" });
-  //   });
+  $(liAndPopup).on("mouseout", function () {
+    $(popup).css({ display: "none" });
+  });
 }
 $("#navbarSection #hamburger svg").on("click", function () {
   $("#navbarSection .phonePopUp").toggleClass("show");
