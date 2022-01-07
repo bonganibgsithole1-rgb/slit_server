a9f1cb9ed1419f8472e3e57c7f205d5f0599acdf
Bongani Sithole <bonganibgsithole@gmail.com>
2022-01-07 14:28:05 +0200

    THe lessons section is almost done only translation effect left

--- app.js
+++ app.js
@@ -5,22 +5,22 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
-  res.render("pages/01_index");
+  res.render("pages/01_index.ejs");
 });
 app.get("/board", function (req, res) {
-  res.render("pages/02_board");
+  res.render("pages/02_board.ejs");
 });
 app.get("/donate", function (req, res) {
-  res.render("pages/03_donate");
+  res.render("pages/03_donate.ejs");
 });
 app.get("/courses", function (req, res) {
-  res.render("pages/04_courses");
+  res.render("pages/04_courses.ejs");
 });
 app.get("/navbar", function (req, res) {
-  res.render("partials/01_navbar");
+  res.render("partials/01_navbar.ejs");
 });
 app.get("/lessons",function(req,res){
-  res.render("pages/05_lessons")
+  res.render("pages/05_lessons.ejs")
 })
 app.listen("3000", function () {
   console.log("Server is connected");
