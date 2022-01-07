1d978bf505321691adf469a1c367da74bdce3d6d
Bongani Sithole <bonganibgsithole@gmail.com>
2022-01-07 18:32:59 +0200

    Lessons 2 route is done which was previously lesson 1

--- app.js
+++ app.js
@@ -16,12 +16,12 @@ app.get("/donate", function (req, res) {
 app.get("/courses", function (req, res) {
   res.render("pages/04_courses.ejs");
 });
-app.get("/navbar", function (req, res) {
-  res.render("partials/01_navbar.ejs");
+app.get("/lessons", function (req, res) {
+  res.render("pages/05_lessons.ejs");
 });
-app.get("/lessons",function(req,res){
-  res.render("pages/05_lessons.ejs")
-})
+app.get("/lessons2", function (req, res) {
+  res.render("partials/13_lessons2.ejs");
+});
 app.listen("3000", function () {
   console.log("Server is connected");
 });
