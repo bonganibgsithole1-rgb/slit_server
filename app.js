ba6bbd345a9533a97b8ca2a6fb9aeaa974d0e366
Bongani Sithole <bonganibgsithole@gmail.com>
2022-01-07 11:23:16 +0200

    The min navbar is done and all the other sections now working on lessons

--- app.js
+++ app.js
@@ -5,21 +5,23 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
-  res.render("pages/01_index.ejs");
+  res.render("pages/01_index");
 });
 app.get("/board", function (req, res) {
-  res.render("pages/02_board.ejs");
+  res.render("pages/02_board");
 });
 app.get("/donate", function (req, res) {
-  res.render("pages/03_donate.ejs");
+  res.render("pages/03_donate");
 });
 app.get("/courses", function (req, res) {
-  res.render("pages/04_courses.ejs");
+  res.render("pages/04_courses");
 });
-
 app.get("/navbar", function (req, res) {
-  res.render("partials/01_navbar.ejs");
+  res.render("partials/01_navbar");
 });
+app.get("/lessons",function(req,res){
+  res.render("pages/05_lessons")
+})
 app.listen("3000", function () {
   console.log("Server is connected");
 });
