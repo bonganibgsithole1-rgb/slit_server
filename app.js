ac0871341d20813831dd54fdf6edcc9f4b722de4
Bongani Sithole <bonganibgsithole@gmail.com>
2022-01-10 12:31:18 +0200

    The C01L01S01 is working correctly

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 let express = require("express");
 let app = express();
+let path = require("path");
 
+// let p = path.join(__dirname, "./public");
+// console.log(p);
+// app.use(express.static(p));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
@@ -16,7 +20,7 @@ app.get("/donate", function (req, res) {
 app.get("/courses", function (req, res) {
   res.render("pages/04_courses.ejs");
 });
-app.get("/lessons", function (req, res) {
+app.get("/courses/C01L01S01", function (req, res) {
   res.render("pages/05_lessons.ejs");
 });
 app.listen("3000", function () {
